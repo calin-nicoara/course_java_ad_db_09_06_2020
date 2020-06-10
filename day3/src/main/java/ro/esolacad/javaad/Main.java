585f3f6e8363f353a8d5e3a292b043619a5a3e85
calin.nicoara <calin.nicoara@esolutions.ro>
2020-06-10 22:42:19 +0300

    Added dependency from module to another.

--- day3/src/main/java/ro/esolacad/javaad/Main.java
+++ day3/src/main/java/ro/esolacad/javaad/Main.java
@@ -2,6 +2,6 @@ package ro.esolacad.javaad;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(UseInOtherProject.test());
     }
 }
---
+++ day3_use/src/main/java/ro/esolacad/javaad/UseInOtherProject.java
@@ -0,0 +1,8 @@
+package ro.esolacad.javaad;
+
+public class UseInOtherProject {
+
+    public static String test() {
+        return "From another module!";
+    }
+}
